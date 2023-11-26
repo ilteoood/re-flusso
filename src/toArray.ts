cdb8cf3155acbeacc26b6f5f3eb0e5efffa29df4
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-11-26 16:41:17 +0100

    feat: toArray

---
+++ src/toArray.ts
@@ -0,0 +1,7 @@
+export const toArray = <T>(destinationArray: T[]): WritableStream<T> => {
+    return new WritableStream<T>({
+        write(chunk) {
+            destinationArray.push(chunk);
+        },
+    })
+};
