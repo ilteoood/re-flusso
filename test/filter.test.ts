7eb062ace0b3b20b30e1d903e297acfcc1349320
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-11-26 19:48:01 +0100

    feat: filter

---
+++ test/filter.test.ts
@@ -0,0 +1,58 @@
+import { setTimeout } from "timers/promises";
+import { describe, expect, it } from "vitest";
+import { filter } from "../src/filter";
+import { fromArray } from "../src/fromArray";
+import { toArray } from "../src/toArray";
+
+describe("filter", () => {
+	it("should filter a stream", async () => {
+		const sourceArray = [1, 2, 3];
+		const destinationArray = [];
+
+		const filterStream = filter<number>((value) => value % 2 === 0);
+
+		await fromArray(sourceArray)
+			.pipeThrough(filterStream)
+			.pipeTo(toArray(destinationArray));
+
+		expect(destinationArray).toEqual([2]);
+	});
+
+	it("should not break for an empty array", async () => {
+		const sourceArray = [];
+		const destinationArray = [];
+
+		const filterStream = filter<number>(() => false);
+		await fromArray(sourceArray)
+			.pipeThrough(filterStream)
+			.pipeTo(toArray(destinationArray));
+
+		expect(destinationArray).toEqual([]);
+	});
+
+	it("should throw if parameter is undefined", async () => {
+		// @ts-expect-error undefined parameter for test
+		const filterStream = filter<number>(undefined);
+
+		expect(
+			fromArray([1])
+				.pipeThrough(filterStream)
+				.pipeTo(toArray([] as number[])),
+		).rejects.toThrow("callbackfn is not a function");
+	});
+
+	it("should handle promises", async () => {
+		const sourceArray = [1, 2, 3];
+		const destinationArray = [];
+
+		const filterStream = filter<number>(async (value) => {
+			await setTimeout(0)
+			return value % 2 === 0
+		});
+		await fromArray(sourceArray)
+			.pipeThrough(filterStream)
+			.pipeTo(toArray(destinationArray));
+
+		expect(destinationArray).toEqual([2]);
+	})
+});
