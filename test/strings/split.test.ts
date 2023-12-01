b0207ea351a70fb3c14ef9b252cb0dae36be8df1
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-12-01 22:32:04 +0100

    chore: test with join

--- test/strings/split.test.ts
+++ test/strings/split.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from "vitest";
 
 import { fromIterable } from "../../src/fromIterable";
 import { pipeline } from "../../src/pipeline";
+import { join } from "../../src/strings/join";
 import { split } from "../../src/strings/split";
 import { toArray } from "../../src/toArray";
 
@@ -24,5 +25,18 @@ describe("split", () => {
 		);
 
 		expect(destinationArray).toEqual(["1", "2", "3"]);
+	});
+
+	test("works well with join", async () => {
+		const destinationArray = [];
+
+		await pipeline(
+			fromIterable(["1-2-3"]),
+			split("-"),
+			join("|"),
+			toArray(destinationArray),
+		);
+
+		expect(destinationArray).toEqual(["1|2|3"]);
 	});
 });
