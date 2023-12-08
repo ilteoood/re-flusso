c663d5f58155e1e9f49d1ee52acac639598d5e88
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-12-08 09:04:21 +0100

    breaking: ndjson parse and stringify

--- test/ndJson/stringify.test.ts
+++ test/ndJson/stringify.test.ts
@@ -1,17 +1,17 @@
 import { describe, expect, it } from "vitest";
 import { fromIterable } from "../../src/fromIterable";
-import { stringifier } from "../../src/ndJson/stringifier";
+import { stringify } from "../../src/ndJson/stringify";
 import { pipeline } from "../../src/pipeline";
 import { toArray } from "../../src/toArray";
 
-describe("ndJson stringifier", () => {
-	describe("stringifier", () => {
+describe("ndJson stringify", () => {
+	describe("stringify", () => {
 		it("should stringify an array", async () => {
 			const destinationArray = [];
 
 			await pipeline(
 				fromIterable([{ value: 1 }, { value: 2 }, { value: 3 }]),
-				stringifier(),
+				stringify(),
 				toArray(destinationArray),
 			);
 
