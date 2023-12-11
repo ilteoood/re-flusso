eec2906ab6a8012e5d0f1555e8881a5be6c1689c
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-12-11 22:20:01 +0100

    fix: example

--- examples/fastify/src/index.mjs
+++ examples/fastify/src/index.mjs
@@ -1,4 +1,6 @@
 import { faker } from "@faker-js/faker";
+import { stringify } from "@ilteoood/re-flusso/ndJson/stringify";
+import { pipeline } from "@ilteoood/re-flusso/pipeline";
 import fastify from "fastify";
 import { Readable } from "node:stream";
 
@@ -6,22 +8,25 @@ const server = fastify();
 
 const itemsToGenerate = 100_000;
 
+function* userGenerator() {
+	for (let i = 1; i <= itemsToGenerate; i++) {
+		yield {
+			name: faker.person.firstName(),
+			surname: faker.person.lastName(),
+			age: faker.number.int({ min: 0, max: 100 }),
+		};
+	}
+}
+
 server.get("/users", (_, reply) => {
-	const readableStream = Readable.from(
-		(function* () {
-			for (let i = 1; i <= itemsToGenerate; i++) {
-				yield JSON.stringify({
-					name: faker.person.firstName(),
-					surname: faker.person.lastName(),
-					age: faker.number.int({ min: 0, max: 100 }),
-				}).concat(i === itemsToGenerate ? "" : "\n");
-			}
-		})(),
+	const readableStream = pipeline(
+		ReadableStream.from(userGenerator()),
+		stringify(),
 	);
 
 	return reply
 		.header("Content-Type", "application/octet-stream")
-		.send(readableStream);
+		.send(Readable.fromWeb(readableStream));
 });
 
 await server.listen({ port: 3000 }, (error, address) => {
