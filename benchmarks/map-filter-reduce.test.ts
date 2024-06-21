6de6a41d967d81d01100ce723a35e4478e8ee227
matteo.pietro.dazzi <matteo.pietro.dazzi@nearform.com>
2024-06-21 06:09:31 +0000

    feat: benchmark

--- benchmarks/map-filter-reduce.test.ts
+++ benchmarks/map-filter-reduce.test.ts
@@ -9,19 +9,18 @@ import { forEach } from "../src/forEach";
 
 describe("map-filter-reduce", () => {
 	const initialArray = new Array(10_000).fill(0).map((_, index) => index);
-	const expectedResult = 99_990_000;
 
 	bench("normal", () => {
-		const result = initialArray
+		initialArray
 			.map((value) => value * 2)
 			.filter((value) => value % 2 === 0)
 			.reduce((accumulator, value) => accumulator + value, 0);
 	});
 
-	bench("re-flusso", () => {
+	bench("re-flusso", async () => {
 		let result = 0;
 
-		pipeline(
+		await pipeline(
 			fromIterable(initialArray),
 			map((value) => value * 2),
 			filter((value) => value % 2 === 0),
